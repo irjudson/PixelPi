4aee45c4fde32e036db29691f8c6896d5ddef300
Ivan R. Judson <irjudson@gmail.com>
2014-04-02 12:30:45 -0700

    reimplementing rate controls for querying whooly

--- moodcloud/moodcloud/settings.py
+++ moodcloud/moodcloud/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
 )
 
 DATA_URL = "http://whooly.cloudapp.net/api/moodcloud"
+UPDATE_FREQUENCY = 5
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
