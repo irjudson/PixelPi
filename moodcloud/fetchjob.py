0fa0a8dff5effb135e7bab3ed56028d20b6d2865
Ivan R. Judson <irjudson@gmail.com>
2014-04-03 14:48:03 -0700

    Updated fixes.

---
+++ moodcloud/fetchjob.py
@@ -0,0 +1,35 @@
+import sys
+import urllib
+import time, datetime
+
+LIMIT = 60
+THRESHOLD = 5
+
+start = datetime.datetime.utcnow()
+print "Starting at: %s" % start
+
+call_time_sum = 0.0
+call_time_count = 0.0
+
+for i in range(12):
+    before = datetime.datetime.utcnow()
+    print "\tRunning: %s" % before
+    response = urllib.urlopen('http://moodcloud.azurewebsites.net/data/')
+    html = response.read()
+    after = datetime.datetime.utcnow() - before
+    call_time_sum += after.microseconds
+    call_time_count += 1.0
+    call_time_avg = (call_time_sum / call_time_count) / 1000000
+    print "\t\tAverage Call Time: %f" % call_time_avg
+    duration = datetime.datetime.utcnow() - start
+    if duration.seconds + max(THRESHOLD, call_time_avg) > LIMIT:
+        break
+    diff = THRESHOLD - after.seconds
+    if diff > 0:
+        print "\t\tSleeping for: %d" % diff
+        time.sleep(diff)
+
+end = datetime.datetime.utcnow()
+print "Ending at: %s" % end
+diff = end-start
+print "Duration: %d" % diff.seconds
