c5aa4897d630daa492989693d29983c463821a58
Ivan R. Judson <irjudson@gmail.com>
2014-04-18 08:20:28 -0700

    working on updates

--- moodcloud.py
+++ moodcloud.py
@@ -330,12 +330,13 @@ def server():
     pixel_output = bytearray(args.num_leds * PIXEL_SIZE)
     server = "http://%s:%d/" % (args.server, args.port)
     while True:
-        all_off()
         serverpath = server + "data/"
         logger.debug("Grabbing next set of sentiment data from %s." % serverpath)
         data = json.loads(urllib2.urlopen(serverpath).read())
+        all_off()
         logger.debug("Data: ")
-        logger.debug(json.dumps(data, sort_keys=True, indent=2))
+        #logger.debug(json.dumps(data, sort_keys=True, indent=2))
+        logger.debug(data)
 
         ip = get_ip(server)
         
