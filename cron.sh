839e31051753321bf733bd29cd67e6c971c58596
Ivan R. Judson <irjudson@gmail.com>
2014-04-21 08:24:30 -0700

    Adding cron script

---
+++ cron.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+
+# Run every minute to make sure the moodcloud is running.
+
+PID="`ps auwx | grep moodcloud.py | grep -v grep`"
+
+if [ "x${PID}" == "x" ]; then
+	echo "Moodcloud not running, restarting."
+else
+	echo "Moodcloud running, exiting."
+fi
+
+exit 0
